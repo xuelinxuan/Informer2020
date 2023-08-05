410c143a8583178214c4cf192bbfc57ca8295a4a
彻 <72044077+xuelinxuan@users.noreply.github.com>
2023-08-05 20:09:42 +0200

    Update data_loader.py

--- data/data_loader.py
+++ data/data_loader.py
@@ -233,8 +233,8 @@ class Dataset_Custom(Dataset):
         num_train = int(len(df_raw)*0.7)
         num_test = int(len(df_raw)*0.2)
         num_vali = len(df_raw) - num_train - num_test
-        border1s = [0, num_train-self.seq_len, len(df_raw)-num_test-self.seq_len]
-        border2s = [num_train, num_train+num_vali, len(df_raw)]
+        border1s = [0, num_train-self.seq_len, len(df_raw)-num_test-self.seq_len]   # trois points de départ
+        border2s = [num_train, num_train+num_vali, len(df_raw)]                     # trois points final 
         border1 = border1s[self.set_type]
         border2 = border2s[self.set_type]
         
