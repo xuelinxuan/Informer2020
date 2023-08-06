3993cd30ea58cf9181584d8ac2b1050cd06f8cd6
彻 <72044077+xuelinxuan@users.noreply.github.com>
2023-08-06 22:00:20 +0200

    Update exp_informer.py

--- exp/exp_informer.py
+++ exp/exp_informer.py
@@ -61,15 +61,15 @@ class Exp_Informer(Exp_Basic):
         args = self.args
 
         data_dict = {
-            'ETTh1':Dataset_ETT_hour,
-            'ETTh2':Dataset_ETT_hour,
-            'ETTm1':Dataset_ETT_minute,
-            'ETTm2':Dataset_ETT_minute,
-            'WTH':Dataset_Custom,
-            'ECL':Dataset_Custom,
-            'Solar':Dataset_Custom,
-            'custom':Dataset_Custom,
-            'custom':Dataset_Custom,
+            'ETTh1'  :Dataset_ETT_hour,
+            'ETTh2'  :Dataset_ETT_hour,
+            'ETTm1'  :Dataset_ETT_minute,
+            'ETTm2'  :Dataset_ETT_minute,
+            'WTH'    :Dataset_Custom,
+            'ECL'    :Dataset_Custom,
+            'Solar'  :Dataset_Custom,
+            'custom' :Dataset_Custom,
+            'custom' :Dataset_Custom,
             'my_data':Dataset_Custom,
         }
         Data = data_dict[self.args.data]     # en focntion de nom pour choisir le type de fucntion
