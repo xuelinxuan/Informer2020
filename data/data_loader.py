8eac2c92ce0c0537f216a10f949b6913cb8926a8
彻 <72044077+xuelinxuan@users.noreply.github.com>
2023-08-06 22:31:55 +0200

    Update data_loader.py

--- data/data_loader.py
+++ data/data_loader.py
@@ -14,8 +14,8 @@ warnings.filterwarnings('ignore')
 
 class Dataset_Custom(Dataset):
     def __init__(self, root_path, flag='train', size=None, 
-                 features='S', data_path='ETTh1.csv', 
-                 target='OT', scale=False, inverse=False, timeenc=0, freq='h', cols=None):
+                 features='S', data_path='my_data.csv', 
+                 target='OT', scale=False, inverse=False, timeenc=0, freq='d', cols=None):
         # size [seq_len, label_len, pred_len]
         # info
         if size == None:
