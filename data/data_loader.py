5779da598dff388300a4f0a6db83d7cef4c34931
彻 <72044077+xuelinxuan@users.noreply.github.com>
2023-08-08 22:02:44 +0200

    Update data_loader.py

--- data/data_loader.py
+++ data/data_loader.py
@@ -88,7 +88,7 @@ class Dataset_Custom(Dataset):
         df_stamp['date'] = pd.to_datetime(df_stamp.date)
         data_stamp = time_features(df_stamp, timeenc=self.timeenc, freq=self.freq)
 
-        self.data_x = data[border1:border2]    # longeur d'ensemble d'entrainemnt ,val, test 
+        self.data_x = data[border1:border2]                  # longeur d'ensemble d'entrainemnt ,val, test 
         if self.inverse:
             self.data_y = df_data.values[border1:border2]
         else:
@@ -132,13 +132,13 @@ class Dataset_Pred(Dataset):
         # size [seq_len, label_len, pred_len]
         # info
         if size == None:
-            self.seq_len = 24*4*4
+            self.seq_len   = 24*4*4
             self.label_len = 24*4
-            self.pred_len = 24*4
+            self.pred_len  = 24*4
         else:
-            self.seq_len = size[0]
+            self.seq_len   = size[0]
             self.label_len = size[1]
-            self.pred_len = size[2]
+            self.pred_len  = size[2]
         # init
         assert flag in ['pred']
         
@@ -148,15 +148,14 @@ class Dataset_Pred(Dataset):
         self.inverse   = inverse
         self.timeenc   = timeenc
         self.freq      = freq
-        self.cols      =cols
+        self.cols      = cols
         self.root_path = root_path
         self.data_path = data_path
         self.__read_data__()
 
     def __read_data__(self):
         self.scaler = StandardScaler()
-        df_raw = pd.read_csv(os.path.join(self.root_path,
-                                          self.data_path))
+        df_raw = pd.read_csv(os.path.join(self.root_path, self.data_path))
         '''
         df_raw.columns: ['date', ...(other features), target feature]
         '''
@@ -167,8 +166,8 @@ class Dataset_Pred(Dataset):
             cols = list(df_raw.columns); cols.remove(self.target); cols.remove('date')
         df_raw = df_raw[['date']+cols+[self.target]]
         
-        border1 = len(df_raw)-self.seq_len
-        border2 = len(df_raw)
+        border1 = len(df_raw)-self.seq_len             # 测试集开始
+        border2 = len(df_raw)                          # 最后
         
         if self.features=='M' or self.features=='MS':
             cols_data = df_raw.columns[1:]
@@ -182,26 +181,28 @@ class Dataset_Pred(Dataset):
         else:
             data = df_data.values
             
-        tmp_stamp = df_raw[['date']][border1:border2]
+        tmp_stamp = df_raw[['date']][border1:border2]    # border1:border2   --   debut de test:fin de test 
         tmp_stamp['date'] = pd.to_datetime(tmp_stamp.date)
+        # pred_dates 的三个系数：date_range() -生成一个固定频率的日期范围
+        #
         pred_dates = pd.date_range(tmp_stamp.date.values[-1], periods=self.pred_len+1, freq=self.freq)
         
         df_stamp = pd.DataFrame(columns = ['date'])
         df_stamp.date = list(tmp_stamp.date.values) + list(pred_dates[1:])
         data_stamp = time_features(df_stamp, timeenc=self.timeenc, freq=self.freq[-1:])
 
-        self.data_x = data[border1:border2]      # border1 fin de train , debut de test border2:fin de test : longeur de test 
+        self.data_x = data[border1:border2]               # border1:border2   --   debut de test:fin de test 
         if self.inverse:
             self.data_y = df_data.values[border1:border2]
         else:
-            self.data_y = data[border1:border2]   # longeur de test 
+            self.data_y = data[border1:border2]           # longeur de test 
         self.data_stamp = data_stamp
     
     def __getitem__(self, index):
         s_begin = index                                    # ensemble d'entraînement point de départ, point final
         s_end = s_begin + self.seq_len                     # ensemble d'entraînement point de départ, point final
         r_begin = s_end - self.label_len                   # 验证集的开始
-        r_end = r_begin + self.label_len + self.pred_len  # 验证集的结束
+        r_end = r_begin + self.label_len + self.pred_len   # 验证集的结束
 
         seq_x = self.data_x[s_begin:s_end]
         if self.inverse:
