3f546b92ec34179d1304cd556253b35905a1ebc1
彻 <72044077+xuelinxuan@users.noreply.github.com>
2023-08-09 16:36:48 +0200

    Update exp_informer.py

--- exp/exp_informer.py
+++ exp/exp_informer.py
@@ -223,8 +223,7 @@ class Exp_Informer(Exp_Basic):
         trues = []
         
         for i, (batch_x,batch_y,batch_x_mark,batch_y_mark) in enumerate(test_loader):
-            pred, true = self._process_one_batch(
-                test_data, batch_x, batch_y, batch_x_mark, batch_y_mark)
+            pred, true = self._process_one_batch(test_data, batch_x, batch_y, batch_x_mark, batch_y_mark)
             preds.append(pred.detach().cpu().numpy())
             trues.append(true.detach().cpu().numpy())
 
